cfe9b2c26b47d648e0d1ae37c5f4ed725716aabe
zhaojie <wlong203@sina.com>
2013-06-12 22:29:52 +0800

    move python_hard_learn to root directory.

--- temp-respository/python_hard_learn/tmp.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-#coding=utf-8
-from sys import argv
-
-script, from_file, in_file = argv
-print "Copy %s to %s." % (from_file, in_file)
-
-open(in_file, 'w').write(open(from_file).read())
