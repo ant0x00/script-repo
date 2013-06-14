bf83683dedde903585a047487cb3ee36cad8e051
miqingren <wlong203@qq.com>
2013-06-14 21:17:57 +0800

    Add exercise script at home to python_hard_learn

---
+++ python_hard_learn/ex5.py
@@ -0,0 +1,20 @@
+# ex5, variables and printing
+# -- coding: utf-8 --
+
+my_name = 'Wang Long'
+my_age = 30 
+my_weight = 65 # kg
+my_height = 170 # cm
+my_eyes = 'Dark'
+my_teeth = 'White'
+my_hair = 'Dark'
+
+print "Let's talk about %s" %my_name
+print "He's %d cm tall." %my_height
+print "He's %d kg heavy." %my_weight
+print "Actually that's not too heavy or too thick."
+print "He's got %s eys and %s hair." %(my_eyes, my_hair)
+print "His teeth are usually %s depending on the tea." % my_teeth
+
+print "本脚本支持中文。。。"
+
---
+++ python_hard_learn/ex9.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+#coding=utf-8
+# ex9.py. 2013-6-11
+# something special in the execises.
+
+days = "Mon Tue Wen Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+with the three double-quotes.
+We'll be able to type as much as we like.
+Even multi-lines if we want.
+"""
