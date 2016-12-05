751fa5a5b9fad12fc5e535a9f306fe2184d2f2fa
赵洁 <zhaojie@augmn.com>
2016-12-05 22:58:47 +0800

    添加第一个demo, http://blog.csdn.net/zzp_403184692/article/details/8184751 （1）：“在Java里面参数传递都是按值传递”这句话的意思是：按值传递是传递的值的拷贝，按引用传递其实传递的是引用的地址值，所以统称按值传递。 （2）：在Java里面只有基本类型和按照下面这种定义方式的String是按值传递，其它的都是按引用传递。就是直接使用双引号定义字符串方式：String str = “Java私塾”;

---
+++ BasicTech/src/TempTest.java
@@ -0,0 +1,22 @@
+/**
+ * Created by wanglong on 16/12/5.
+ */
+public class TempTest {
+    private void test1(A a) {
+        a = new A();
+        a.age = 20;
+        System.out.println("test1方法中的age=" + a.age);
+    }
+
+    public static void main(String[] args) {
+        TempTest t = new TempTest();
+        A a = new A();
+        a.age = 10;
+        t.test1(a);
+        System.out.println("main方法中的age=" + a.age);
+    }
+}
+
+class A {
+    public int age = 0;
+}
