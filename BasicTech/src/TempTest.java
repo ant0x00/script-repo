240baf5c7bd568bc345bfb67045b388c6dd2e691
赵洁 <zhaojie@augmn.com>
2017-02-26 20:01:05 +0800

    args在命令行下可以为main传入参数

--- BasicTech/src/TempTest.java
+++ BasicTech/src/TempTest.java
@@ -2,18 +2,13 @@
  * Created by wanglong on 16/12/5.
  */
 public class TempTest {
-    static int b;
     static {
-        TempTest.b = 8;
-        System.out.println(b);
-        System.out.println("static block is called before main()");
+        System.out.println("static block is called before main()...");
     }
     public static void main(String[] args) {
-//        TempTest t = new TempTest();
-//        A a = new A();
-//        a.age = 10;
-//        t.test1(a);
-//        System.out.println("main方法中的age=" + a.age);
+        for (int i=0; i<args.length;i++){
+            System.out.println(args[i]);
+        }
     }
 
     private void test1(A a) {
