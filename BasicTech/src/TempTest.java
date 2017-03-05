a3d62e4f7f7434e4a2f1236996bb37f8e86550fe
赵洁 <zhaojie@augmn.com>
2017-03-05 12:00:58 +0800

    新增例子

--- BasicTech/src/TempTest.java
+++ BasicTech/src/TempTest.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 /**
  * Created by wanglong on 16/12/5.
  */
@@ -11,14 +13,82 @@ public class TempTest {
     static {
         System.out.println("static block is called before main()...");
     }
+
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
         }
-        System.out.println("test1方法中的staticVarOfA=" + A.staticVarOfA);
+//        System.out.println("test1方法中的staticVarOfA=" + A.staticVarOfA);
+        test4();
     }
 
-    private void test1(A a) {
+    /**
+     * 中间缓存变量机制实例
+     * temp=j;
+     * j=j+1;
+     * j=temp;
+     */
+    static void test4() {
+        int j = 0;
+        for (int i = 0; i < 100; i++) {
+            j = j++;
+        }
+        System.out.println(j);
+    }
+
+    /**
+     * Java中字符只有一种形式存在，Unicode。
+     * 分两种：面向字符I/O,面向字节流I/O
+     * 面向字符Reader/Writer类，面向字节InputStream/OutPutStream
+     */
+    static void test3() {
+        File f = new File("test");
+        try {
+            FileOutputStream fop = new FileOutputStream(f);
+            OutputStreamWriter writer = new OutputStreamWriter(fop, "UTF-8");
+            // 构建OutputStreamWriter对象,参数可以指定编码,默认为操作系统默认编码,windows上是gbk
+
+            writer.append("中文输入");
+            // 写入到缓冲区
+
+            writer.append("\r\n");
+            //换行
+
+            writer.append("English");
+            // 刷新缓存冲,写入到文件,如果下面已经没有写入的内容了,直接close也会写入
+
+            writer.close();
+            //关闭写入流,同时会把缓冲区内容写入文件,所以上面的注释掉
+
+            fop.close();
+            // 关闭输出流,释放系统资源
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void test2() {
+        String str = "";
+        // 使用 System.in 创建 BufferedReader
+        BufferedReader br = new BufferedReader(new
+                InputStreamReader(System.in));
+        System.out.println("输入字符, 按下 'q' 键退出。");
+        // 读取字符
+        do {
+            try {
+                str = (String) br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(str);
+        } while (!"end".equals(str));
+    }
+
+    private static void test1(A a) {
         a = new A();
         a.age = 20;
         System.out.println("test1方法中的age=" + a.age);
